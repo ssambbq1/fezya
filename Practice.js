6108a711085e06422e352d1cd053bbe45e8d5bae
Jihun Choi <ssambbqcjh@gmail.com>
2022-12-04 13:57:47 +0900

    first commit

---
+++ Practice.js
@@ -0,0 +1,14 @@
+class Person {
+    constructor(name) {
+        this.name = name;
+            }
+
+        static sayHi() {
+            console.log('Hi!');
+        }
+}
+
+Person.sayHi();
+
+//const me = New Person('Lee');
+//me.sayHi();
